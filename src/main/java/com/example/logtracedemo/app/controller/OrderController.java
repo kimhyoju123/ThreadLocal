51cf042620332bb9769797b1cceec75e0ef7527c
kimhyoju123 <qjqmf217@naver.com>
2023-08-23 15:45:03 +0900

    Add OrderController Code

--- src/main/java/com/example/logtracedemo/app/controller/OrderController.java
+++ src/main/java/com/example/logtracedemo/app/controller/OrderController.java
@@ -21,6 +21,42 @@ public class OrderController {
 
     @GetMapping("/")
     public String getOrder() {
+//        logTrace.begin("OrderController.getOrder()");
+//
+//        String result = orderService.getOrder();
+//
+//        logTrace.end("OrderController.getOrder()");
+//
+//        return result;
+
+        // 템플릿 메소드 패턴
+//        AbstractTemplate abstractTemplate = new ConcreteController(logTrace, orderService);
+//        return abstractTemplate.execute("OrderController.getOrder()");
+
+        // 익명 클래스로 템플릿 메소드 패턴
+//        AbstractTemplate abstractTemplate = new AbstractTemplate(logTrace) {
+//            @Override
+//            protected String call() {
+//                return orderService.getOrder();
+//            }
+//        };
+//        return abstractTemplate.execute("OrderController.getOrder()");
+
+        // 전략 패턴
+//        Strategy strategy = new StrategyConcreteController(orderService);
+        // 익명 클래스
+//        Strategy strategy = new Strategy() {
+//            @Override
+//            public String call() {
+//                return orderService.getOrder();
+//            }
+//        };
+        // 람다식
+//        Strategy strategy = () -> {
+//            return orderService.getOrder();
+//        };
+//        Context context = new Context(logTrace, strategy);
+//        return context.execute("OrderController.getOrder()");
 
         // Strategy 인터페이스를 파라미터 받는 전략패턴 == 템플릿 콜백 패턴
         InterfaceParamContext context = new InterfaceParamContext(logTrace);
